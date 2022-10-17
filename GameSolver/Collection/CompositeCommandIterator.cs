2bc1fec9c1ac0776202b08fb57f5f2856b97ed29
chuckboliver <patpumhak@gmail.com>
2022-10-18 01:37:32 +0700

    :bug: fix: Fix iterator did not get all value

--- GameSolver/Collection/CompositeCommandIterator.cs
+++ GameSolver/Collection/CompositeCommandIterator.cs
@@ -4,29 +4,41 @@
     {
         private readonly CompositeCommand _compositeCommand;
         private int _currentPosition;
+        private int _round;
         private IIterator<GameAction> _currentIterator;
 
         public CompositeCommandIterator(CompositeCommand compositeCommand)
         {
             _compositeCommand = compositeCommand;
             _currentPosition = 0;
+            _round = 0;
             _currentIterator = _compositeCommand.Commands[0].CommandIterator();
         }
 
         public GameAction GetNext()
         {
+            GameAction action = _currentIterator.GetNext();
+
+            int commandsCount = _compositeCommand.Commands.Count;
             while (!_currentIterator.HasMore())
             {
                 _currentPosition++;
+
+                if (_currentPosition == commandsCount)
+                {
+                    _currentPosition %= commandsCount;
+                    _round++;
+                }
+
                 _currentIterator = _compositeCommand.Commands[_currentPosition].CommandIterator();
             }
 
-            return _currentIterator.GetNext();
+            return action;
         }
 
         public bool HasMore()
         {
-            return _currentPosition < _compositeCommand.Commands.Count - 1 || _currentIterator.HasMore();
+            return _round < _compositeCommand.Quantity;
         }
     }
 }

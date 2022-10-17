0357c25877cfe1741fe6cb03d19a12493069b882
chuckboliver <patpumhak@gmail.com>
2022-10-18 01:37:54 +0700

    :construction: feat: Add some testcase

--- GameSolver/Program.cs
+++ GameSolver/Program.cs
@@ -42,13 +42,23 @@ Console.WriteLine();
 // Testcase composite rle
 Console.WriteLine("Testcase Composite RLE");
 
-var a1 = new Command(GameAction.Left, 1);
+var a1 = new Command(GameAction.Left, 3);
 var a2 = new Command(GameAction.Right, 1);
 var a3 = new Command(GameAction.Left, 2);
-var c1 = new CompositeCommand(7);
+var c1 = new CompositeCommand(3);
 c1.AddCommand(a1);
 c1.AddCommand(a2);
 c1.AddCommand(a3);
+
+Console.WriteLine(c1.ToFullString());
+
+var n = c1.CommandIterator();
+while (n.HasMore())
+{
+    Console.Write((char)n.GetNext());
+}
+Console.WriteLine();
+
 
 var x1 = new Command(GameAction.Left, 1);
 var x2 = new Command(GameAction.Right, 1);
@@ -68,3 +78,33 @@ CompositeCommand result = rle2.Encode();
 Console.WriteLine($"After: {result.ToRegex()}");
 
 Console.WriteLine($"FullString: {result.ToFullString()}");
+
+Console.WriteLine();
+
+var kk = new CompositeCommand();
+
+
+// Testcase suffix array construction
+static void SuffixArrayConstructionTest()
+{
+    Console.WriteLine("Testcase suffix array construction");
+
+    var con = new CompositeCommand();
+    var con1 = new CompositeCommand(1);
+    var con2 = new CompositeCommand(1);
+
+    var c1 = new Command(GameAction.Up, 1);
+    var c2 = new Command(GameAction.Up, 1);
+    var c3 = new Command(GameAction.Left, 1);
+    var c4 = new Command(GameAction.Up, 1);
+    con1.AddCommand(c1);
+    con1.AddCommand(c2);
+    con2.AddCommand(c3);
+    con2.AddCommand(c4);
+    con.AddCommand(con1);
+    con.AddCommand(con2);
+
+    var suffixArray = new SuffixArray(con);
+}
+
+SuffixArrayConstructionTest();

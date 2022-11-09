b8386c872a5606789b50cbb17628ecc48a9e97be
chuckboliver <patpumhak@gmail.com>
2022-11-09 11:48:41 +0700

    :truck: refactor: Move IntVector2 type to  IntVector2.cs

--- GameSolver/Game/Board.cs
+++ GameSolver/Game/Board.cs
@@ -2,23 +2,6 @@
 
 namespace GameSolver.Game
 {
-    public struct IntVector2
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-
-        public IntVector2(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public override string ToString()
-        {
-            return $"x: {X}, y: {Y}";
-        }
-    }
-
     public class PlayerPosition : ICloneable
     {
         public IntVector2 Position { get; set; }

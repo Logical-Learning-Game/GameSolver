0a41dc5a9deaadef13aa0282e207f22906b7d89a
chuckboliver <patpumhak@gmail.com>
2022-11-15 00:10:10 +0700

    :truck: refactor: Move type Direction to Direction.cs

--- GameSolver/Game/Game.cs
+++ GameSolver/Game/Game.cs
@@ -2,14 +2,6 @@
 
 namespace GameSolver.Game
 {
-    public enum Direction
-    {
-        Up = 0,
-        Left = 1,
-        Down = 2,
-        Right = 3
-    }
-
     public sealed class Game
     {
         // Tile Numeric

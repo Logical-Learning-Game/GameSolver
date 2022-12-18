7bd543ff1cd9a5dd468245eabaca0643c451bd69
chuckboliver <patpumhak@gmail.com>
2022-12-18 14:12:53 +0700

    :sparkles: feat: Add Iterative Deepening Depth First Search

---
+++ GameSolver/Solver/IterativeDeepeningDepthFirstSearch.cs
@@ -0,0 +1,32 @@
+﻿using GameSolver.Core;
+using GameSolver.Core.Action;
+
+namespace GameSolver.Solver;
+
+public sealed class IterativeDeepeningDepthFirstSearch : ISolver
+{
+    private readonly Game _game;
+
+    public IterativeDeepeningDepthFirstSearch(Game game)
+    {
+        _game = game;
+    }
+
+    public IEnumerable<IGameAction> Solve()
+    {
+        int depth = 0;
+
+        while (true)
+        {
+            var dfsSolver = new DepthFirstSearch(_game, depth);
+            List<IGameAction> solution = dfsSolver.SolveDefaultStrategy();
+
+            if (solution.Count > 0)
+            {
+                return solution;
+            }
+
+            depth++;
+        }
+    }
+}

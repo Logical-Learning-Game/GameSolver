548ef1921e403ca6af1f1cb47ab4c7739cffca03
chuckboliver <patpumhak@gmail.com>
2022-12-18 14:44:53 +0700

    :sparkles: feat: Add AllSolutionAtDepth method
    - add IsCycle method for cycle checking in search graph

--- GameSolver/Solver/BreadthFirstSearch.cs
+++ GameSolver/Solver/BreadthFirstSearch.cs
@@ -97,5 +97,67 @@ public sealed class BreadthFirstSearch : ISolver
         return new List<IGameAction>();
     }
 
+    public List<List<IGameAction>> AllSolutionAtDepth(int depth)
+    {
+        var allSolutions = new List<List<IGameAction>>();
+        
+        var initialState = new State(_game);
+        var frontier = new Queue<BFSStateData>();
+        var bfsData = new BFSStateData(null, null, initialState, 0);
+        frontier.Enqueue(bfsData);
+
+        if (initialState.IsSolved() && bfsData.Depth == depth)
+        {
+            allSolutions.Add(bfsData.Solution());
+            return allSolutions;
+        }
+
+        while (frontier.Count > 0)
+        {
+            BFSStateData data = frontier.Dequeue();
+            State currentState = data.State;
+
+            if (data.Depth > depth)
+            {
+                return allSolutions;
+            }
+
+            if (!IsCycle(data))
+            {
+                foreach (IGameAction action in currentState.LegalGameActions())
+                {
+                    State childState = State.Update(currentState, action);
+                
+                    var childStateData = new BFSStateData(action, data, childState, data.Depth + 1);
+            
+                    if (childStateData.Depth == depth && childState.IsSolved())
+                    {
+                        allSolutions.Add(childStateData.Solution());
+                        continue;
+                    }
+            
+                    frontier.Enqueue(childStateData);
+                }
+            }
+        }
+
+        return new List<List<IGameAction>>();
+    }
+    
+    private static bool IsCycle(BFSStateData stateData)
+    {
+        State state = stateData.State;
+        BFSStateData currentStateData = stateData;
+        while (currentStateData.PrevState != null)
+        {
+            if (state.ZobristHash == currentStateData.PrevState.State.ZobristHash)
+            {
+                return true;
+            }
+
+            currentStateData = currentStateData.PrevState;
+        }
+
+        return false;
     }
 }

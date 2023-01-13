486e46fc892519e905707479d1030fdb6e724078
chuckboliver <patpumhak@gmail.com>
2023-01-13 19:28:16 +0700

    :sparkles: feat: Add NullAction for action that do nothing

---
+++ GameSolver/Core/Action/NullAction.cs
@@ -0,0 +1,14 @@
+﻿namespace GameSolver.Core.Action;
+
+public class NullAction : IGameAction
+{
+    public void Do(State state)
+    {
+        
+    }
+
+    public void Undo(State state)
+    {
+        
+    }
+}

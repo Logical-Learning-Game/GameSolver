2e929b2263ef5f58d64115a85c28ebc596aabd35
chuckboliver <patpumhak@gmail.com>
2023-01-24 11:38:35 +0700

    :sparkles: feat: Add ObserveConditionAction
    - StartAction delegate to ObserverConditionAction
    - Sealed NullAction

---
+++ GameSolver/Core/Action/ObserveConditionAction.cs
@@ -0,0 +1,52 @@
+﻿namespace GameSolver.Core.Action;
+
+public sealed class ObserveConditionAction : IGameAction
+{
+    private readonly IGameAction _gameAction;
+    private bool _isObserved;
+    private bool _haveOldCondition;
+
+    public ObserveConditionAction(IGameAction gameAction)
+    {
+        _gameAction = gameAction;
+    }
+
+    public void Do(State state)
+    {
+        _gameAction.Do(state);
+        
+        Vector2Int playerPos = state.PlayerPosition;
+        int currentTile = state.Board[playerPos.Y, playerPos.X];
+
+        if (TileComponent.Conditional.In(currentTile))
+        {
+            _isObserved = true;
+
+            if (state.Conditions == 1)
+            {
+                _haveOldCondition = true;
+            }
+            
+            state.Conditions = 1;
+        }
+    }
+
+    public void Undo(State state)
+    {
+        if (!_isObserved)
+        {
+            return;
+        }
+
+        state.Conditions = _haveOldCondition ? 1 : 0;
+
+        _gameAction.Undo(state);
+    }
+
+    public override string ToString()
+    {
+        string? toStringResult = _gameAction.ToString();
+        
+        return string.IsNullOrEmpty(toStringResult) ? string.Empty : toStringResult;
+    }
+}
--- GameSolver/Core/Action/StartAction.cs
+++ GameSolver/Core/Action/StartAction.cs
@@ -1,9 +1,26 @@
 ﻿namespace GameSolver.Core.Action;
 
-public sealed class StartAction : NullAction
+public sealed class StartAction : IGameAction
 {
+    private readonly ObserveConditionAction _observe;
+    
+    public StartAction()
+    {
+        _observe = new ObserveConditionAction(new NullAction());
+    }
+    
+    public void Do(State state)
+    {
+        _observe.Do(state);
+    }
+
+    public void Undo(State state)
+    {
+        _observe.Undo(state);
+    }
+
     public override string ToString()
     {
-        return "Start";
+        return "s";
     }
 }
